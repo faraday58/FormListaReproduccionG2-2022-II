a71f868b79e4333142ceecb29dd5302d711762c5
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-03-16 12:31:09 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace FormListaReproduccionG2_2022_II
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbArtista = new System.Windows.Forms.Label();
+            this.lbAlbum = new System.Windows.Forms.Label();
+            this.lbCancion = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarCancionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(45, 118);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(359, 439);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(560, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Artista";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(560, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Álbum";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(560, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Canción";
+            // 
+            // lbArtista
+            // 
+            this.lbArtista.AutoSize = true;
+            this.lbArtista.Location = new System.Drawing.Point(674, 118);
+            this.lbArtista.Name = "lbArtista";
+            this.lbArtista.Size = new System.Drawing.Size(79, 29);
+            this.lbArtista.TabIndex = 1;
+            this.lbArtista.Text = "label1";
+            // 
+            // lbAlbum
+            // 
+            this.lbAlbum.AutoSize = true;
+            this.lbAlbum.Location = new System.Drawing.Point(674, 192);
+            this.lbAlbum.Name = "lbAlbum";
+            this.lbAlbum.Size = new System.Drawing.Size(79, 29);
+            this.lbAlbum.TabIndex = 1;
+            this.lbAlbum.Text = "label1";
+            // 
+            // lbCancion
+            // 
+            this.lbCancion.AutoSize = true;
+            this.lbCancion.Location = new System.Drawing.Point(674, 292);
+            this.lbCancion.Name = "lbCancion";
+            this.lbCancion.Size = new System.Drawing.Size(79, 29);
+            this.lbCancion.TabIndex = 1;
+            this.lbCancion.Text = "label1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(36, 36);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1176, 45);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarCancionesToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(128, 41);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // cargarCancionesToolStripMenuItem
+            // 
+            this.cargarCancionesToolStripMenuItem.Name = "cargarCancionesToolStripMenuItem";
+            this.cargarCancionesToolStripMenuItem.Size = new System.Drawing.Size(403, 48);
+            this.cargarCancionesToolStripMenuItem.Text = "Cargar Canciones";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 656);
+            this.Controls.Add(this.lbCancion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbAlbum);
+            this.Controls.Add(this.lbArtista);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbArtista;
+        private System.Windows.Forms.Label lbAlbum;
+        private System.Windows.Forms.Label lbCancion;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarCancionesToolStripMenuItem;
+    }
+}
+
---
+++ Musica.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace FormListaReproduccionG2_2022_II
+{
+    class Musica
+    {
+        #region Atributos
+        private string cancion;
+        private string artista;
+        private string album;
+        #endregion
+
+        #region Propiedades
+        public string Cancion { 
+            get => cancion; 
+            set            
+            {
+                if( value == "")
+                {
+                    cancion = "El borrego";
+                }
+                else
+                {
+                    cancion = value;
+                }
+
+            }
+            
+        }
+        public string Artista { 
+            get => artista;
+            set
+            {
+                if( value == "")
+                {
+                    artista = "Café Tacuba";
+                }else
+                {
+                    artista = value;
+                }
+                
+            }
+        }
+        public string Album { 
+            get => album; 
+            set
+            {
+                if ( value == "")
+                {
+                    album = "RE";
+                }
+                else
+                {
+                    album = value;
+                }
+                
+            }
+                
+        }
+        #endregion
+
+
+
+
+
+
+    }
+}
