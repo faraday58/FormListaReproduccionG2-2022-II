65afd5332a9ce179d7dd02f99d2d8b8474e8344e
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-03-30 14:28:34 -0600

    Subiendo una imagen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,14 +40,18 @@ namespace FormListaReproduccionG2_2022_II
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cargarCancionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.agregarCancionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // lstbCanciones
             // 
             this.lstbCanciones.FormattingEnabled = true;
             this.lstbCanciones.ItemHeight = 29;
-            this.lstbCanciones.Location = new System.Drawing.Point(45, 118);
+            this.lstbCanciones.Location = new System.Drawing.Point(79, 105);
             this.lstbCanciones.Name = "lstbCanciones";
             this.lstbCanciones.Size = new System.Drawing.Size(359, 439);
             this.lstbCanciones.TabIndex = 0;
@@ -56,7 +60,7 @@ namespace FormListaReproduccionG2_2022_II
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(560, 118);
+            this.label1.Location = new System.Drawing.Point(594, 177);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(79, 29);
             this.label1.TabIndex = 1;
@@ -65,7 +69,7 @@ namespace FormListaReproduccionG2_2022_II
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(560, 192);
+            this.label2.Location = new System.Drawing.Point(594, 251);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(81, 29);
             this.label2.TabIndex = 1;
@@ -74,7 +78,7 @@ namespace FormListaReproduccionG2_2022_II
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(560, 292);
+            this.label3.Location = new System.Drawing.Point(594, 351);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(101, 29);
             this.label3.TabIndex = 1;
@@ -83,7 +87,7 @@ namespace FormListaReproduccionG2_2022_II
             // lbArtista
             // 
             this.lbArtista.AutoSize = true;
-            this.lbArtista.Location = new System.Drawing.Point(674, 118);
+            this.lbArtista.Location = new System.Drawing.Point(708, 177);
             this.lbArtista.Name = "lbArtista";
             this.lbArtista.Size = new System.Drawing.Size(79, 29);
             this.lbArtista.TabIndex = 1;
@@ -92,7 +96,7 @@ namespace FormListaReproduccionG2_2022_II
             // lbAlbum
             // 
             this.lbAlbum.AutoSize = true;
-            this.lbAlbum.Location = new System.Drawing.Point(674, 192);
+            this.lbAlbum.Location = new System.Drawing.Point(708, 251);
             this.lbAlbum.Name = "lbAlbum";
             this.lbAlbum.Size = new System.Drawing.Size(79, 29);
             this.lbAlbum.TabIndex = 1;
@@ -101,7 +105,7 @@ namespace FormListaReproduccionG2_2022_II
             // lbCancion
             // 
             this.lbCancion.AutoSize = true;
-            this.lbCancion.Location = new System.Drawing.Point(674, 292);
+            this.lbCancion.Location = new System.Drawing.Point(708, 351);
             this.lbCancion.Name = "lbCancion";
             this.lbCancion.Size = new System.Drawing.Size(79, 29);
             this.lbCancion.TabIndex = 1;
@@ -114,7 +118,7 @@ namespace FormListaReproduccionG2_2022_II
             this.archivoToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1176, 47);
+            this.menuStrip1.Size = new System.Drawing.Size(1558, 45);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -124,28 +128,48 @@ namespace FormListaReproduccionG2_2022_II
             this.cargarCancionesToolStripMenuItem,
             this.agregarCancionesToolStripMenuItem});
             this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
-            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(128, 43);
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(128, 41);
             this.archivoToolStripMenuItem.Text = "Archivo";
             // 
             // cargarCancionesToolStripMenuItem
             // 
             this.cargarCancionesToolStripMenuItem.Name = "cargarCancionesToolStripMenuItem";
-            this.cargarCancionesToolStripMenuItem.Size = new System.Drawing.Size(374, 48);
+            this.cargarCancionesToolStripMenuItem.Size = new System.Drawing.Size(390, 48);
             this.cargarCancionesToolStripMenuItem.Text = "Cargar Canciones";
             this.cargarCancionesToolStripMenuItem.Click += new System.EventHandler(this.cargarCancionesToolStripMenuItem_Click);
             // 
             // agregarCancionesToolStripMenuItem
             // 
             this.agregarCancionesToolStripMenuItem.Name = "agregarCancionesToolStripMenuItem";
-            this.agregarCancionesToolStripMenuItem.Size = new System.Drawing.Size(403, 48);
+            this.agregarCancionesToolStripMenuItem.Size = new System.Drawing.Size(390, 48);
             this.agregarCancionesToolStripMenuItem.Text = "Agregar Canciones";
             this.agregarCancionesToolStripMenuItem.Click += new System.EventHandler(this.agregarCancionesToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FormListaReproduccionG2_2022_II.Properties.Resources.american_teen_album;
+            this.pictureBox1.Location = new System.Drawing.Point(843, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(388, 385);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1556, 440);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1176, 656);
+            this.ClientSize = new System.Drawing.Size(1558, 656);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lbCancion);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lbAlbum);
@@ -159,6 +183,8 @@ namespace FormListaReproduccionG2_2022_II
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,6 +203,8 @@ namespace FormListaReproduccionG2_2022_II
         private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem cargarCancionesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem agregarCancionesToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
